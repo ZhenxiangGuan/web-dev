afa01a810426ad854c4ad4868aa98aa8c30ec1c9
Steven <steveng@Stevens-Macbook-Pro.local>
2022-03-21 22:51:53 -0400

    a6 React.js sp22

--- src/components/labs/classes/index.js
+++ src/components/labs/classes/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import './index.css';
 const Classes = () => {
   const color = 'blue';
-  const dangerous = false;
+  const dangerous = true;
   return(
       <div>
         <h2>Classes</h2>
