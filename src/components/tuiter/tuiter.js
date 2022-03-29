388715a664e166f0e630919dd62d136fa8d69497
Steven <steveng@Stevens-Macbook-Pro.local>
2022-03-28 23:37:08 -0400

    a7 Redux sp22

--- src/components/tuiter/tuiter.js
+++ src/components/tuiter/tuiter.js
@@ -1,6 +1,4 @@
 import React from "react";
-// import {Link} from "react-router-dom";
-// import ExploreScreen from "./ExploreScreen/ExploreScreen";
 import {Outlet} from "react-router-dom";
 import NavigationSidebar from "./NavigationSidebar";
 import whoReducer from "./reducers/who-reducer";
