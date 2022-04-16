b10a15d2748553e936e2fe29b09c890671ed70ee
Steven <steveng@Stevens-Macbook-Pro.local>
2022-04-15 22:11:10 -0400

    final

--- src/components/tuiter/whats-happening.js
+++ src/components/tuiter/whats-happening.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import "./whats-happening.css"
-import { useDispatch }
+import {useDispatch}
   from "react-redux";
 import {
   createTuit
@@ -12,36 +12,41 @@ const WhatsHappening = () => {
   const [newTuit, setNewTuit] =
       useState({tuit: 'New tuit'});
 
-
   return (
-    <div className="pos-relative">
-      <div className="row">
-        <div className="col-1">
-          <img alt="post logo" className="rounded-circle wd-avatar-45" src="/tuiter/gg.jpg" />
-        </div>
-        <div className="col-11">
+      <div className="pos-relative">
+        <div className="row">
+          <div className="col-1">
+            <img alt="post logo" className="rounded-circle wd-avatar-45"
+                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1024px-Google_%22G%22_Logo.svg.png"/>
+          </div>
+          <div className="col-11">
           <textarea className="col wd-text-area" placeholder="What's happening?"
                     onChange={(e) =>
-                        setNewTuit({...newTuit,
-                          tuit: e.target.value})}>
+                        setNewTuit({
+                          ...newTuit,
+                          tuit: e.target.value
+                        })}>
           </textarea>
+          </div>
         </div>
-      </div>
-      <div className="row">
-        <div className="col-1"></div>
-        <div className="col">
-          <i className="fa-solid fa-image wd-icon-color" />
-          <i className="fa-solid fa-chart-line wd-margin-left-4 wd-icon-color" />
-          <i className="fa-solid fa-face-smile wd-margin-left-4 wd-icon-color" />
-          <i className="fa-solid fa-calendar wd-margin-left-4 wd-icon-color" />
-        </div>
-        <div className="col">
-          <button className="mt-3 mb-3 btn btn-primary  rounded-pill fa-pull-right" onClick={() =>
-              createTuit(dispatch, newTuit)}>Tweet</button>
-        </div>
+        <div className="row">
+          <div className="col-1"></div>
+          <div className="col">
+            <i className="fa-solid fa-image wd-icon-color"/>
+            <i className="fa-solid fa-chart-line wd-margin-left-4 wd-icon-color"/>
+            <i className="fa-solid fa-face-smile wd-margin-left-4 wd-icon-color"/>
+            <i className="fa-solid fa-calendar wd-margin-left-4 wd-icon-color"/>
+          </div>
+          <div className="col">
+            <button
+                className="mt-3 mb-3 btn btn-primary  rounded-pill fa-pull-right"
+                onClick={() =>
+                    createTuit(dispatch, newTuit)}>Tweet
+            </button>
+          </div>
 
+        </div>
       </div>
-    </div>
   );
 }
 export default WhatsHappening;
